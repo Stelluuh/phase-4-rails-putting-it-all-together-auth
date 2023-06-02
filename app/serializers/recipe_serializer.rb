abc8c6eac43f187a7eb1e4331c778ae2daf54950
stella <sftamesis13@gmail.com>
2023-06-02 11:09:07 -0700

    complete

---
+++ app/serializers/recipe_serializer.rb
@@ -0,0 +1,9 @@
+class RecipeSerializer < ActiveModel::Serializer
+  attributes :id, :title, :instructions, :minutes_to_complete
+  has_one :user
+
+  def minutes_to_complete
+    object.minutes_to_complete.to_i
+  end
+  
+end
